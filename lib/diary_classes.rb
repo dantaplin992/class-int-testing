089d6f0efd2369a9968d25556938b5cd166b815f
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-03-09 12:08:16 +0000

    diary complete

--- lib/diary_classes.rb
+++ lib/diary_classes.rb
@@ -23,20 +23,24 @@ class Diary
     return word_count
   end
 
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # if the user were to read all entries in the diary.
+  def reading_time(wpm)
+    total_words = count_words.sum
+    return total_words / wpm
   end
 
   def find_best_entry_for_reading_time(wpm, minutes)
-        # `wpm` is an integer representing the number of words the user can read
-        # per minute.
-        # `minutes` is an integer representing the number of minutes the user
-        # has to read.
-    # Returns an instance of diary entry representing the entry that is closest 
-    # to, but not over, the length that the user could read in the minutes they
-    # have available given their reading speed.
+    return_entry = nil
+    min_diff = minutes
+    max_words = wpm * minutes
+    for entry in @entries do
+      time_to_read = entry.reading_time(wpm)
+      time_diff = minutes - time_to_read
+      if time_diff < min_diff && time_diff >= 0
+        return_entry = entry
+        min_diff = time_diff
+      end
+    end
+    return return_entry
   end
 end
 
@@ -51,17 +55,16 @@ class DiaryEntry
   end
 
   def contents
-    # Returns the contents as a string
+    @contents
   end
 
   def count_words
     @contents.split(/\s/).count
   end
 
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+  def reading_time(wpm)
+    words = @contents.split(" ").count
+    words / wpm
   end
 
   def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
--- spec/diary_classes_spec.rb
+++ spec/diary_classes_spec.rb
@@ -21,7 +21,17 @@ RSpec.describe 'diary_classes integration' do
     diary = Diary.new
     diary.add(DiaryEntry.new("08 Mar", "Tested some classes"))
     diary.add(DiaryEntry.new("09 Mar", "Tested some integrations"))
-    expect(Diary.reading_time(1)).to eq 6
-    expect(Diary.reading_time(3)).to eq 2
+    expect(diary.reading_time(1)).to eq 6
+    expect(diary.reading_time(3)).to eq 2
+  end
+
+  it 'returns diary entry closest to total that could be read, in minutes' do
+    diary = Diary.new
+    entry1 = DiaryEntry.new("08 Mar", "Today I tested some classes")
+    entry2 = DiaryEntry.new("09 Mar", "Today I tested some multiple class integrations")
+    diary.add(entry1)
+    diary.add(entry2)
+    expect(diary.find_best_entry_for_reading_time(1, 6)).to eq entry1
+    expect(diary.find_best_entry_for_reading_time(1, 8)).to eq entry2
   end
 end
